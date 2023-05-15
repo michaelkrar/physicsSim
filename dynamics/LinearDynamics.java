6990b7b827f7701851aa3f1dd3d1f8d846df1c39
michaelkrar <michael.krar1@gmail.com>
2023-05-15 12:14:14 -0400

    Unimplement Force and write initial dynamics class

--- dynamics/LinearDynamics.java
+++ dynamics/LinearDynamics.java
@@ -2,11 +2,28 @@ package dynamics;
 
 import java.util.ArrayList;
 
+import geometry.Translation2d;
+
 public class LinearDynamics {
     
-    private ArrayList<Force> mFreeBody;
+    private ArrayList<Translation2d> mFreeBody;
 
-    public LinearDynamics () {
-
+    public LinearDynamics (ArrayList<Translation2d> forces) {
+        this.mFreeBody = forces;
     }
+
+    public Translation2d netForce (ArrayList<Translation2d> forces) {
+        if(forces.size() != 1) { //we add the last force in the list to the first one, until we only have 1 force left
+            forces.get(0).add(forces.get(forces.size()-1));
+            forces.remove(forces.size()-1);
+            return netForce();
+        } else {
+            return forces.get(0);
+        }
+    }
+
+    public Translation2d netForce () {
+        return netForce(mFreeBody);
+    }
+
 }
